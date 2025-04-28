ab84aa1df69e6c2313b39d5639766f3da7676fc6
Kurt Clifford <kclif9@gmail.com>
2025-04-28 21:31:38 +1000

    Fix logging configuration in constants file for improved debugging

--- custom_components/actronair_neo/const.py
+++ custom_components/actronair_neo/const.py
@@ -1,10 +1,11 @@
 """Constants used by Actron Air Neo integration."""
 
-import logging
 from datetime import timedelta
+import logging
 
 from homeassistant.const import Platform
 
+logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__package__)
 DOMAIN = "actronair_neo"
 PLATFORM = [Platform.CLIMATE, Platform.COVER, Platform.SENSOR, Platform.SWITCH]
